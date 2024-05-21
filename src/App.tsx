7b2b6df1b5f1734edb6b900af8887f95ccc45898
moj14 <moj14@srtp.io>
2024-05-21 18:33:50 +0300

    configs edit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,6 @@
 import React from "react"
 
-enum Colors {
-    red,
-    blue,
-    green,
-    noMother
-}
-
-interface IAnimal {
-    povadki: string
-    color: Colors
-}
-
-interface IStupidStudent extends IAnimal{
-    iq: number
-    name: string | number
-    tasks: number
-    canBeNotStupid: boolean
-    lessons?: number
-    hasMom: boolean
-}
-
 function App() {
-
-    const stupidStudent: IStupidStudent = {
-        iq: -12,
-        name: "evgeny",
-        tasks: 999,
-        canBeNotStupid: false,
-        hasMom: false,
-        povadki: "еврей",
-        color: Colors.red
-    }
-
-    const stupidStudents: IStupidStudent[] = [stupidStudent, stupidStudent, stupidStudent]
-
-
-    console.log(stupidStudents)
 
   return (
     <div className={""}>
